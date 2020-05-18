987e9020501484cf43a7ba116669a07c527c590e
“あなたのgit”shin <zhenfugaoye86@gmail.com>
2020-05-18 16:05:05 +0900

    ミニアプリ　ストップウォッチ完成

---
+++ MyStopwatch/js/main.js
@@ -0,0 +1,70 @@
+'use strict'
+
+{
+  const timer =document.getElementById('timer');
+  const start =document.getElementById('start');
+  const stop =document.getElementById('stop');
+  const reset =document.getElementById('reset');
+
+let startTime;
+let timeoutId;
+let elapsedTime = 0;
+
+function countUp(){
+  // console.log(Date.now() - startTime);
+ const d = new Date(Date.now() - startTime + elapsedTime);
+ const m = String(d.getMinutes()).padStart(2,'0');
+ const s = String(d.getSeconds()).padStart(2,'0');
+ const ms = String(d.getMilliseconds()).padStart(3,'0');
+ timer.textContent = `${m}:${s}.${ms}`;
+
+  timeoutId = setTimeout(()=> {
+    countUp();
+  },10);
+}
+
+function setButtonstateInitial(){
+  start.classList.remove('inactive');
+     stop.classList.add('inactive');
+     reset.classList.add('inactive');
+}
+function setButtonstateRunning(){
+    start.classList.add('inactive');
+    stop.classList.remove('inactive');
+    reset.classList.add('inactive');
+}
+function setButtonstateStopped(){
+      start.classList.remove('inactive');
+      stop.classList.add('inactive');
+      reset.classList.remove('inactive');
+}
+
+setButtonstateInitial();
+
+  start.addEventListener('click', () => {
+    if(start.classList.contains('inactive')){
+      return;
+    }
+    setButtonstateRunning();
+    startTime =Date.now();
+    countUp();
+  });
+
+  stop.addEventListener('click', () => {
+    if(stop.classList.contains('inactive')){
+      return;
+    }
+    setButtonstateStopped();
+    clearTimeout(timeoutId);
+    elapsedTime += Date.now() - startTime;
+  });
+
+  reset.addEventListener('click', () => {
+    if(reset.classList.contains('inactive')){
+      return;
+    }
+    setButtonstateInitial();
+    timer.textContent ='00:00.000';
+    elapsedTime = 0;
+  });
+}
